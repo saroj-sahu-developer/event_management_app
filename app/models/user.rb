544dbf38c5a5c5c206edabb003c07d9a0ad568e3
Saroj Sahu <sahusarojkumar2020@gmail.com>
2025-03-28 14:59:15 +0530

    Add association and validation in User model

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :events, foreign_key: :organizer_id, dependent: :nullify
+  has_many :bookings, foreign_key: :customer_id, dependent: :nullify
+
+  validates :name, :email, :role, presence: true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :role, inclusion: { in: %w(customer, organizer), message: "must be 'customer' or 'organizer'" }
+end
